9e9896a21ea9058b9fe83ad698a92ad670c0f86d
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2021-12-03 11:33:15 +0200

    Adding Individual MealItem and [some MeaItemForm - not completed yet]

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,17 @@
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  return (
+    <form>
+      <div>
+        <h2>Amount</h2>
+        <div>1</div>
+      </div>
+      <div>
+        <button>+Add</button>
+      </div>
+    </form>
+  );
+};
+
+export default MealItemForm;
